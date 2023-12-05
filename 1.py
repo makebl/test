cd3a23d5722a0f418ec4cc77d44554de8b0c7b9e
马可 <78850005+makebl@users.noreply.github.com>
2023-12-05 23:19:53 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,26 +1,38 @@
 import json
 
 def merge_libraries(library1, library2):
-    merged_data = {
-        "libraries": [library1, library2]
-    }
-    return merged_data
+    apps1 = library1.get('apps', [])
+    apps2 = library2.get('apps', [])
+    merged_apps = apps1 + apps2
+    merged_library = library1.copy()
+    merged_library['apps'] = merged_apps
+    return merged_library
 
 def main():
-    # 读取第一个 JSON 文件
-    with open("test.json", "r", encoding="utf-8") as file1:
-        library1 = json.load(file1)
+    file1_path = 'test.json'
+    file2_path = 'apps.json'
 
-    # 读取第二个 JSON 文件
-    with open("apps.json", "r", encoding="utf-8") as file2:
-        library2 = json.load(file2)
+    # Ensure test.json exists and is not empty
+    try:
+        with open(file1_path, 'r', encoding='utf-8') as test_file:
+            test_content = json.load(test_file)
+            if 'apps' not in test_content:
+                test_content['apps'] = []
+    except (json.JSONDecodeError, FileNotFoundError) as e:
+        print(f"Error loading {file1_path}: {e}")
+        test_content = {'apps': []}
 
-    # 合并两个 IPA 库
-    merged_library = merge_libraries(library1, library2)
+    try:
+        with open(file2_path, 'r', encoding='utf-8') as file2:
+            library2 = json.load(file2)
+    except (json.JSONDecodeError, FileNotFoundError) as e:
+        print(f"Error loading {file2_path}: {e}")
+        library2 = {'apps': []}
 
-    # 将合并后的数据写入新的 JSON 文件
-    with open("merged_library.json", "w", encoding="utf-8") as output_file:
-        json.dump(merged_library, output_file, ensure_ascii=False, indent=4)
+    merged_library = merge_libraries(test_content, library2)
+
+    with open('merged_library.json', 'w', encoding='utf-8') as merged_file:
+        json.dump(merged_library, merged_file, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     main()
